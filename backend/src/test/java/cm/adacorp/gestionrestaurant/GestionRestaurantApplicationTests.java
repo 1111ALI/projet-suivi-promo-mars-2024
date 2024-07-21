fe183d1686ac73b5475290327db0a6b8aa304e20
Andreas MOUZONG <andreasmouzong@gmail.com>
2024-07-21 15:30:21 +0200

    #3 Initialisation du projet et ajout du Dockerfile et compose.yaml

---
+++ backend/src/test/java/cm/adacorp/gestionrestaurant/GestionRestaurantApplicationTests.java
@@ -0,0 +1,13 @@
+package cm.adacorp.gestionrestaurant;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestionRestaurantApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
